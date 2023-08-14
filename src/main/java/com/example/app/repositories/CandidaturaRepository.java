e715318582f9049900ef92f06756041e3f1f5d1e
Vinicius Valverde <valverdevinicius@protonmail.com>
2023-08-14 16:54:03 -0300

    Implementação dos endpoints /localizacao e /hisig10/candidaturas do método listarCandidaturas

--- src/main/java/com/example/app/repositories/CandidaturaRepository.java
+++ src/main/java/com/example/app/repositories/CandidaturaRepository.java
@@ -1,9 +1,12 @@
 package com.example.app.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.app.model.entities.Candidatura;
 
 public interface CandidaturaRepository extends JpaRepository<Candidatura, Long> {
-
+	List<Candidatura> findByCandidatoLocalizacao(@Param("localizacao") String localizacao);
 }
