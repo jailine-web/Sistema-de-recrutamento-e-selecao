ef2c9eb13e5126870d6cdbd4d5d558ab7007198d
Jailine <jay.santana964@gmail.com>
2023-08-22 18:48:02 -0300

    Merge

--- src/main/java/com/example/app/controller/VagaController.java
+++ src/main/java/com/example/app/controller/VagaController.java
@@ -27,50 +27,49 @@ import com.example.app.repositories.QuestionarioRepository;
 import com.example.app.services.VagaService;
 
 @RestController
-@RequestMapping(value = "/hisig10/vagas")
+@RequestMapping(value="/hisig10/vagas")
 public class VagaController {
-
+	
 	@Autowired
 	private VagaService vs;
-
+	
 	@Autowired
 	private QuestionarioRepository questionarioRepository;
-
+	
 	@GetMapping
-	public List<Vaga> buscarTodos() {
+	public List<Vaga> buscarTodos(){
 		List<Vaga> listaVagas = vs.buscarTodos();
-
+		
 		return listaVagas;
 	}
-
+	
 	@PostMapping
-	public ResponseEntity<Vaga> inserirVaga(@RequestBody Vaga vaga) {
+	public ResponseEntity<Vaga> inserirVaga(@RequestBody Vaga vaga){
 		vaga = vs.inserirVaga(vaga);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(vaga.getId()).toUri();
 		return ResponseEntity.created(uri).body(vaga);
 	}
-
-	@GetMapping(value = "/{id}")
+	
+	@GetMapping(value="/{id}")
 	public ResponseEntity<Vaga> buscarPorId(@PathVariable Integer id) {
 		Vaga v = vs.buscarPorId(id);
 		return ResponseEntity.ok().body(v);
 	}
-
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<Vaga> atualizarVaga(@PathVariable Integer id, @RequestBody Vaga vaga) {
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Vaga> atualizarVaga(@PathVariable Integer id, @RequestBody Vaga vaga){
 		vaga = vs.atualizarVaga(id, vaga);
-		return ResponseEntity.ok().body(vaga);
+		return ResponseEntity.ok().body(vaga);	
 	}
-
+	
 	@PostMapping("/questionarios/{vagaId}")
-	public ResponseEntity<?> criarQuestionarioParaVaga(@RequestBody QuestionarioDTO questionarioDTO,
-			@PathVariable Integer vagaId) {
+	public ResponseEntity<?> criarQuestionarioParaVaga(@RequestBody QuestionarioDTO questionarioDTO, @PathVariable Integer vagaId){
 		try {
 			Vaga vaga = vs.buscarPorId(vagaId);
-
+			
 			Questionario questionario = new Questionario();
 			questionario.setTitulo(questionarioDTO.getTitulo());
-
+			
 			List<Pergunta> perguntas = new ArrayList<>();
 			for (PerguntaDTO perguntaDTO : questionarioDTO.getPerguntas()) {
 				Pergunta pergunta = new Pergunta();
@@ -81,27 +80,27 @@ public class VagaController {
 			}
 			questionario.setPerguntas(perguntas);
 			questionario.setVaga(vaga);
-
+			
 			questionarioRepository.save(questionario);
-
+			
 			return ResponseEntity.status(HttpStatus.CREATED).build();
-
+			
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 	}
-
+	
 	@GetMapping("/questionarios")
 	public ResponseEntity<List<Questionario>> buscarQuestionariosDeTodasVagas() {
 		List<Questionario> questionarios = questionarioRepository.findAll();
 		return ResponseEntity.ok().body(questionarios);
 	}
-
+	
 	@GetMapping("/questionarios/{id}")
-	public ResponseEntity<?> buscarQuestionarioPorId(@PathVariable Long id) {
+	public ResponseEntity<?> buscarQuestionarioPorId(@PathVariable Long id){
 		try {
 			Optional<Questionario> questionarioOptional = questionarioRepository.findById(id);
-
+			
 			if (questionarioOptional.isPresent()) {
 				Questionario questionario = questionarioOptional.get();
 				return ResponseEntity.ok().body(questionario);
@@ -112,12 +111,12 @@ public class VagaController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 	}
-
+	
 	@DeleteMapping("/questionarios/{id}")
-	public ResponseEntity<?> excluirQuestionario(@PathVariable Long id) {
+	public ResponseEntity<?> excluirQuestionario(@PathVariable Long id){
 		try {
 			Optional<Questionario> questionarioOptional = questionarioRepository.findById(id);
-
+			
 			if (questionarioOptional.isPresent()) {
 				questionarioRepository.delete(questionarioOptional.get());
 				return ResponseEntity.noContent().build();
@@ -127,18 +126,16 @@ public class VagaController {
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
-
 	}
 
-	@PutMapping(value = "{id}/fecharvaga")
+	@PutMapping(value ="{id}/fecharvaga")
 	public ResponseEntity<Void> FecharVaga(@PathVariable Integer id) {
 		Vaga v1 = vs.fecharVaga(id);
 		return ResponseEntity.ok().build();
 	}
-
-	@GetMapping(value = "/{id}/vagaativa")
+	
+	@GetMapping(value="/{id}/vagaativa")
 	public ResponseEntity<Long> vagaAtiva(@PathVariable Integer id) {
 		Long diasAtiva = vs.vagaAtiva(id);
 		return ResponseEntity.ok().body(diasAtiva);
-	}
 }
